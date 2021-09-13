ae96f06f915368fe4ad93736137fc487aac27d41
nvstco <ivanisaiahserato@gmail.com>
2021-09-13 14:04:18 +0800

    Add comments
    Added some comments for readability.  Fixed the yes/no command.

--- commands/roleformat.js
+++ commands/roleformat.js
@@ -23,21 +23,24 @@
                 .setTimestamp()
             .setColor(message.guild.me.displayHexColor)
 
-            let aufm_wait = await message.channel.send(aufmEmbed);
+        // wait until the entire process is finished to delete the embed
+        let aufm_wait = await message.channel.send(aufmEmbed);
 
-        // collects all the messages sent and modifies them 
+        // collects all the messages sent and modifies them for the role section
         const collector = new Discord.MessageCollector(message.channel, filter, { max:10, time: 120*1000 });
         collector.on('collect', m => {
 
+            // check if the message sent is an existing role
             var rolename = m.guild.roles.cache.find(role => role.name === m.content);
             var roleid = message.guild.roles.cache.get(m.content);
             var t_msg = m.content;
 
-            // if the message is "stop", it will stop collecting messages
+            // if the message is "stop", the collector will stop collecting messages
             if (m.content == "stop") {
                 m.delete();
                 collector.stop();
             }
+
             // if the message contains a role, the role name, or the id
             else if (m.mentions.roles.first() || rolename != undefined || roleid){
                 
@@ -52,8 +55,7 @@
                 // creates an embed that will reply if the role has been added
                 var valid = new Discord.MessageEmbed()
                     .setDescription(`**📢  Successfully added role #${ctr}:** ${t_msg}`)
-                    .setColor("2f3136")
-                    
+                    .setColor("2f3136");
                 message.channel.send(valid).then(r => r.delete({timeout: 5*1000}));
                 ctr++;
             }
@@ -64,6 +66,7 @@
             }
         });
 
+        // once the collector finished collecting messages
         collector.on('end', collected => {
 
             // splits type into an array
@@ -85,7 +88,10 @@
                     .setThumbnail(load)
                 .setTimestamp()
                 
+            // defines the size to be used for modification later on
             c_size = collected.size;
+
+            // if there are no roles in the menu
             if (collected.size-1 == 0) 
                 fnEmbed.addField("\u200b", "No roles to show.")
             // if there is only one role in the menu
@@ -98,65 +104,78 @@
                     {name: "Column 02", value:type_n2, inline: true}
                 )
             }
-
             fnEmbed.addField('\u200b', endmsg)
+
+            // sends the embed, but asks for modifications (if any)
             message.channel.send(fnEmbed).then(msgg => {
             
-
+            // inform to add parameters, then delete user message immediately afterwards
             message.channel.send("Would you like to edit some parameters?").then(r => r.delete({timeout: 10*1000}))
             message.delete();
 
             // waits for a message to be sent, and then when collected, executes
             message.channel.awaitMessages(filter, {max: 1, time: 10000}).then(collected => {
 
-                var c_parameter = [ "author", "description", "color", "footer", "header", "filler"];
+                // parameters to be edited in 
+                var c_parameter = [ "author", "description", "color hex", "footer", "header", "filler"];
                 var contents = "", ctr2 = 0;
 
-                // if message is "cancel", process will stop
+                // if the message sent is yes, execute
                 if(collected.first().content == "yes" || collected.first().content == "y") {
 
+                    // prepares the message to be sent, with the parameters inside
                     var inst_msg = `**Enter your ${c_parameter[ctr2]} message.** [Type 'retain' to keep it the same, or 'empty' to clear out the field.]`;
+                    // sends the message, but waits for input for modifications
                     message.channel.send(inst_msg).then(msg =>{
-                            
+                        
+                        // collects all the messages sent and modifies them for the params section
                         const collectorParam = new Discord.MessageCollector(message.channel, filter, { max:5, time: 60*1000 });
                         collectorParam.on('collect', m => {
                             
+                            // fixes showing the "filler" text when the message gets edited.
                             if (ctr2 < 4) 
                                 msg.edit(`**Enter your ${c_parameter[ctr2+1]} message.** [Type 'retain' to keep it the same, or 'empty' to clear out the field.]`);
                             else {
                                 msg.edit(`Parameters added.`).then(r => r.delete({timeout: 3*1000}));
                             }
 
+                            // concatenates the content into a single string, to be filtered later on.
                             contents += m.content + "\n";
                             m.delete();
                             ctr2++;
                         });
 
+                        // once the collector finishes collecting messages
                         collectorParam.on('end', collected => {
                             
-                            // filters
+                            // splits the content into an array
                             cont_array = contents.split("\n");
-
-                            // push
                             var header = "Column";
+
+                            // creates the new embed to be edited.
                             const fn_reembed = new Discord.MessageEmbed()
                                 .setFooter("Hyperspace Formatting", load)
                                 .setThumbnail(load)
                                 .setTimestamp()
 
+                            // checks if the author section has modifications
                             if (cont_array[0] == "retain") fn_reembed.setAuthor(author);
                             else if (cont_array[0] != "empty") fn_reembed.setAuthor(`❱❱  ${cont_array[0]}`);
 
+                            // checks if the description section has modifications
                             if (cont_array[1] == "retain") fn_reembed.setDescription(description);
                             else if (cont_array[1] != "empty") fn_reembed.setDescription(cont_array[1]);
 
+                            // checks if the color hex section has modifications
                             if (cont_array[2] == "retain") fn_reembed.setColor(message.guild.me.displayHexColor);
                             else if (cont_array[2] != "empty") fn_reembed.setColor(cont_array[2]);
                             else if (cont_array[2] == "empty") fn_reembed.setColor("#2f3136");
 
+                            // checks if the header section has modifications
                             if (cont_array[4] == "empty") header = "/u200b";
                             else if (cont_array[4] != "empty") header = cont_array[4];
 
+                            // utilizing c-size and type as global variables, to be inserted into the new embed
                             if (c_size-1 == 0) 
                                 fn_reembed.addField("\u200b", "No roles to show.")
                             // if there is only one role in the menu
@@ -170,24 +189,27 @@
                                 )
                             }
 
+                            // checks if the footer section has modifications
                             if (cont_array[3] == "retain") fn_reembed.addField('\u200b', endmsg);
                             else if (cont_array[3] != "empty") fn_reembed.addField('\u200b', cont_array[3]);
 
+                            // edits the embed into a new one with modifications, and deletes the first message.
                             msgg.edit(fn_reembed);
-  
+                            aufm_wait.delete();
                         });
-                    
                     });
                 }
 
-                else if(collected.first().content == "no") 
-                    return message.channel.send("Setup complete.").then(r => r.delete({timeout: 3*1000}));
-                
-                }).catch(err => {
+                // if the message sent is no, stop the command.
+                if(collected.first().content == "no" || collected.first().content == "n") {
+                    message.channel.send("Setup complete.").then(r => r.delete({timeout: 3*1000}));
+                    aufm_wait.delete();
+                }
+
+            // catches the error and logs it in.
+            }).catch(err => {
                     console.log(err);
                 });
-
-                aufm_wait.delete();
             });
         });
     }
@@ -196,6 +218,3 @@
         name: "rfmt",
         aliases: ['rf']
     }
-
-    // add support for custom prefix
-    // add support for fnEmbed customization
