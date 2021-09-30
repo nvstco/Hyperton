ef7b8b0222ab18425b29b5b3a5fa0c054591f03e
nvstco <ivanisaiahserato@gmail.com>
2021-09-30 18:40:02 +0800

    Move colors.json to colors.txt
    Migrated the role IDs to a .txt file for cleaner code.

--- commands/layout/cformat.js
+++ commands/layout/cformat.js
@@ -1,22 +1,19 @@
 
     const Discord = require('discord.js')
+    const fs = require('fs');
+
     module.exports.run = async (client, message, args) => {
 
         if(message.deletable) message.delete();
-        const { author, description, load, logo, endmsg } = require('../../resources/color.json')
+        const { author, description, load, logo, endmsg } = require('../../resources/formats.json')
 
-        const { c_cloud, c_wheat, c_salmon, c_crimson, c_burgundy, c_indigo, c_phtalo, c_seagreen, c_teal, c_moss } = require('../../resources/color.json')
-        var color_a = `${c_cloud}${c_wheat}${c_salmon}${c_crimson}${c_burgundy}${c_indigo}${c_phtalo}${c_seagreen}${c_teal}${c_moss}`;
-
-        const { c_gold, c_bronze, c_magenta, c_lavender, c_periwinkle, c_purple, c_azure, c_condense, c_smoke, c_marsblack } = require('../../resources/color.json')
-        var color_b = `${c_gold}${c_bronze}${c_magenta}${c_lavender}${c_periwinkle}${c_purple}${c_azure}${c_condense}${c_smoke}${c_marsblack}`;
-
+        const color_x = fs.readFileSync('./resources/colors.txt').toString().split('/');
         const hpEmbed = new Discord. MessageEmbed ()  
             .setAuthor(author)
             .setDescription(description)
             .addFields(
-                {name: "Hues 01", value:color_a, inline: true},
-                {name: "Hues 02", value:color_b, inline: true}
+                {name: "Hues 01", value:color_x[0], inline: true},
+                {name: "Hues 02", value:color_x[1], inline: true}
             )
             .addField('\u200b', endmsg)
             .setColor(message.guild.me.displayHexColor)
